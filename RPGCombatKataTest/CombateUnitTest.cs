bfd472eae3e511217d7c7e2c4393114e0eee2a98
Felipe Dutra <servidor.soulsoft@gmail.com>
2023-03-16 13:11:06 -0300

    Iteration Two

--- RPGCombatKataTest/CombateUnitTest.cs
+++ RPGCombatKataTest/CombateUnitTest.cs
@@ -26,7 +26,7 @@ namespace RPGCombatKataTest
         [Fact]
         public void Danificar500()
         {
-            //Gera um dano de 1000
+            //Gera um dano de 500
             jogador = Combate.Danificar(jogador, adversario, 500);
             //Verifica a situação de vida do personagem
             Assert.Equal(adversario.Saude, 500);
@@ -36,7 +36,7 @@ namespace RPGCombatKataTest
         [Fact]
         public void Danificar50()
         {
-            //Gera um dano de 1000
+            //Gera um dano de 50
             jogador = Combate.Danificar(jogador, jogador, 50);
             //Verifica a situação de vida do personagem
             Assert.Equal(jogador.Saude, 1000);
@@ -47,7 +47,7 @@ namespace RPGCombatKataTest
         public void Curar100()
         {
             jogador.Saude = 100;
-            //Gera um dano de 1000
+            //Gera uma cura de 100
             jogador = Combate.Curar(jogador, jogador, 100);
             //Verifica a situação de saude do personagem
             Assert.Equal(jogador.Saude, 200);
@@ -57,7 +57,7 @@ namespace RPGCombatKataTest
         [Fact]
         public void Curar50()
         {
-            //Gera um dano de 1000
+            //Gera uma cura de 50
             jogador = Combate.Curar(jogador, jogador, 50);
             //Verifica a situação de saude do personagem
             Assert.Equal(jogador.Saude, 1000);
@@ -68,10 +68,34 @@ namespace RPGCombatKataTest
         public void Curar40()
         {
             adversario.Saude = 100;
-            //Gera um dano de 1000
+            //Gera uma cura de 100
             jogador = Combate.Curar(jogador, adversario, 40);
             //Verifica a situação de saude do personagem
             Assert.Equal(adversario.Saude, 100);
+        }
+
+        //Verifica se o jogador que recebe dano de oponente com diferenca de nivel maior que 5 tem o dano aumentado
+        [Fact]
+        public void Danificar500AumentarDanoPorNivel()
+        {
+            //Seta que o nivel do jogador que faz a interacao é 5 niveis maior do que o nivel do adversario
+            jogador.Nivel = 6;
+            //Gera um dano de 500
+            var recebeInteracao = Combate.Danificar(jogador, adversario, 500);
+            //Verifica a situação de saude do personagem
+            Assert.Equal(recebeInteracao.Saude, 250);
+        }
+
+        //Verifica se o jogador que recebe dano de oponente com diferenca de nivel menor que 5 tem o dano reduzido
+        [Fact]
+        public void Danificar500DiminuirDanoPorNivel()
+        {
+            //Seta que o nivel do jogador que recebe a interacao é 5 niveis maior do que o nivel do adversario
+            adversario.Nivel = 6;
+            //Gera um dano de 500
+            var recebeInteracao = Combate.Danificar(jogador, adversario, 500);
+            //Verifica a situação de saude do personagem
+            Assert.Equal(recebeInteracao.Saude, 750);
         }
     }
 }
