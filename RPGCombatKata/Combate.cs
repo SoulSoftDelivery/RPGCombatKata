8af865083fa2fa6dae094b6697ba332d3bd7ca26
Felipe Dutra <servidor.soulsoft@gmail.com>
2023-03-15 23:48:58 -0300

    Primeiro Deploy

---
+++ RPGCombatKata/Combate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RPGCombatKata
+{
+    public class Personagem
+    {
+        public string Nome { get; set; }
+        public int Saude { get; set; } = 1000;
+        public int Nivel { get; set; } = 1;
+        public bool Vivo { get; set; } = true;
+    }
+
+    public static class Combate
+    {
+        public static Personagem Danificar(Personagem personagem, int totalDanificar)
+        {
+            if (personagem.Vivo)
+            {
+                if ((personagem.Saude - totalDanificar) <= 0)
+                {
+                    personagem.Saude = 0;
+                    personagem.Vivo = false;
+                }
+                else
+                {
+                    personagem.Saude -= totalDanificar;
+                }
+            }
+
+            return personagem;
+        }
+
+        public static Personagem Curar(Personagem personagem, int totalCurar)
+        {
+            if (personagem.Vivo)
+            {
+                if ((personagem.Saude + totalCurar) >= 1000)
+                {
+                    personagem.Saude = 1000;
+                }
+                else
+                {
+                    personagem.Saude += totalCurar;
+                }
+            }
+
+            return personagem;
+        }
+    }
+}
---
+++ RPGCombatKataTest/CombateUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using RPGCombatKata;
+using System.Security;
+using Xunit;
+
+namespace RPGCombatKataTest
+{
+    public class CombateUnitTest
+    {
+        //Cria um personagem para o teste
+        Personagem personagem = new Personagem { Nome = "Goku" };
+
+        //Verifica se o personagem morreu apos o dano
+        [Fact]
+        public void Danificar1000()
+        {
+            //Gera um dano de 1000
+            personagem = Combate.Danificar(personagem, 1000);
+            //Verifica a situação de vida do personagem
+            Assert.Equal(personagem.Vivo, false);
+        }
+
+        //Verifica se o personagem continua vivo apos o dano
+        [Fact]
+        public void Danificar500()
+        {
+            //Gera um dano de 1000
+            personagem = Combate.Danificar(personagem, 500);
+            //Verifica a situação de vida do personagem
+            Assert.Equal(personagem.Vivo, true);
+        }
+
+        //Verifica se o nivel de cura do personagem foi adicionado
+        [Fact]
+        public void Curar100()
+        {
+            personagem.Saude = 100;
+            //Gera um dano de 1000
+            personagem = Combate.Curar(personagem, 100);
+            //Verifica a situação de saude do personagem
+            Assert.Equal(personagem.Saude, 200);
+        }
+
+        //Verifica se o nivel de cura do personagem não excedeu o limite
+        [Fact]
+        public void Curar50()
+        {
+            //Gera um dano de 1000
+            personagem = Combate.Curar(personagem, 1000);
+            //Verifica a situação de saude do personagem
+            Assert.Equal(personagem.Saude, 1000);
+        }
+    }
+}
